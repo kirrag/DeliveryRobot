75efdccd89bbf99a454a41d78338fe375593f843
Garrik Ten <ten@otux.ru>
2023-04-23 01:06:38 +0300

    DeliveryRobot2 v1.0

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,80 +1,90 @@
 package ru.netology;
 
 import java.util.Random;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class Main {
-    public static String generateRoute(String letters, int length) {
+	public static String generateRoute(String letters, int length) {
 		Random random = new Random();
 		StringBuilder route = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-   	        route.append(letters.charAt(random.nextInt(letters.length())));
-        }
-   	    return route.toString();
+		for (int i = 0; i < length; i++) {
+			route.append(letters.charAt(random.nextInt(letters.length())));
+		}
+		return route.toString();
 	}
 
 	public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
 	public static void main(String[] args) {
-		int maxKey = 0;
-		int maxValue = 0;
 
 		long startTs = System.currentTimeMillis();
 
-		new Thread( () -> {
-			int count = 0;
-			while (!Thread.interrupted()) {
-				if (sizeToFreq.containsKey(count)) {
-					for (int key : sizeToFreq.keySet()) {
-						int value = sizeToFreq.get(key);
-						if (value > maxValue) {
-							maxValue = value;
-							maxKey = key;
+		List<Thread> threads = new ArrayList<>();
+
+		Thread threadPrint = new Thread(() -> {
+			int maxKey = 0;
+			int maxValue = 0;
+			try {
+				while (!Thread.interrupted()) {
+					synchronized (sizeToFreq) {
+						sizeToFreq.wait();
+						for (int key : sizeToFreq.keySet()) {
+							int value = sizeToFreq.get(key);
+							if (value > maxValue) {
+								maxValue = value;
+								maxKey = key;
+							}
 						}
+						System.out.println(
+								"Самое частое количество повторений " + maxKey + "(встретилось " + maxValue + " раз)");
 					}
-					return maxKey;
-				} else {
-					System.out.println("Список пуст.");
 				}
+			} catch (InterruptedException e) {
+				return;
 			}
 		});
 
 		for (int n = 0; n < 1000; n++) {
 
 			String text = generateRoute("RLRFR", 100);
+			threads.add(new Thread(() -> {
 
-
-
-
-			new Thread(() -> {
-
+				int count = 0;
 
 				for (int i = 0; i < text.length(); i++) {
-					if (text.charAt(i) == 'R' ) {
+					if (text.charAt(i) == 'R') {
 						count++;
 					}
 				}
 
-				synchronized(sizeToFreq) {
+				synchronized (sizeToFreq) {
 					if (sizeToFreq.containsKey(count)) {
 						sizeToFreq.put(count, sizeToFreq.get(count) + 1);
 					} else {
 						sizeToFreq.put(count, 1);
 					}
+					sizeToFreq.notify();
 				}
-			}).start();
+			}));
 		}
+		threadPrint.start();
+		threads.forEach(thread -> thread.start());
+		threads.forEach(thread -> {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
 
 		long endTs = System.currentTimeMillis();
 
 		System.out.println("Time: " + (endTs - startTs) + "ms");
 
-	
-		System.out.println("Самое частое количество повторений " + maxKey + "(встретилось " + maxValue +" раз) Другие размеры:");
+		threadPrint.interrupt();
 
-		for (int key : sizeToFreq.keySet()) {
-			System.out.println("- " + key + "(" + sizeToFreq.get(key) + ")");
-		}
 	}
 }
